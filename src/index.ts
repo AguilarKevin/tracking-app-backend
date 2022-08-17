e88dae4b55f92ed62ebc6ef4f319e8212f6d4733
aguilarkevin <kevinaguilar0813@gmail.com>
2022-08-16 23:10:18 -0600

    fixes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {app, start} from './server.js'
 
 start(Number(process.env.PORT) || 3000)
 
-app.get('/users', async (req, res) => {
+app.get('/users', async (_, res) => {
   const users = await prisma.user.findMany()
   res.json(users)
 })
