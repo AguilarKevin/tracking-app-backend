a8fad5a2d7c6827f5d30d4ec3d34f014a5101748
aguilarkevin <kevinaguilar0813@gmail.com>
2022-08-16 22:09:39 -0600

    base user model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import {PrismaClient} from '@prisma/client'
 import 'dotenv/config'
 
-import {start} from './server.js'
+import {app, start} from './server.js'
 
 start(Number(process.env.PORT) || 3000)
+
+const prisma = new PrismaClient()
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
